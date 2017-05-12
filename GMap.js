7dbcba700c83fda689b968237386fb9ef4659b0d
Theodore Morley <theodore.morley@mail.mcgill.ca>
2017-05-12 13:36:39 -0400

    Moved JavaScript to seperate file.

---
+++ GMap.js
@@ -0,0 +1,6 @@
+function myMap() {
+    var mapProp= {
+        center:new google.maps.LatLng(43.529872, -72.361059),zoom:15,
+    };
+    var map=new google.maps.Map(document.getElementById("Map"),mapProp);
+}
